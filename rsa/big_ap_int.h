9ea6824cfa8f58e7fb74ad3b33dcbc835605c0df
Jacob Glueck <jng55@cornell.edu>
2018-11-15 09:59:05 -0500

    broken rsa sad

--- rsa/big_ap_int.h
+++ rsa/big_ap_int.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define AP_INT_MAX_W 4096
+#define AP_INT_MAX_W 2048
 #include <ap_int.h>
--- rsa/fpga_rsa.cc
+++ rsa/fpga_rsa.cc
@@ -1,20 +1,35 @@
 #include "fpga_rsa.h"
+#include "bignum.h"
+
+typedef Bignum<MAX_BIT_LEN / 32> RsaBignum;
 
 ap_uint<MAX_BIT_LEN> fpga_powm(ap_uint<MAX_BIT_LEN> base, ap_uint<MAX_BIT_LEN> exponent, ap_uint<MAX_BIT_LEN> modulus) {
   if (modulus == 1) {
     return 0;
   }
+/*
+  RsaBignum result(1);
+  RsaBignum b(base);
+  RsaBignum e(exponent);
+  RsaBignum m(modulus);
+*/
   ap_uint<MAX_BIT_LEN> result = 1;
-  base = base % modulus;
-  while (exponent > 0) {
+  ap_uint<MAX_BIT_LEN> b = base;
+  ap_uint<MAX_BIT_LEN> e = exponent;
+  ap_uint<MAX_BIT_LEN> m = modulus;
+
+  b = b % m;
+  while (e > 0) {
     if (exponent[0] == 1) {
-      result = (result * base) % modulus;
+      result = (result * b) % m;
     }
-    exponent = exponent >> 1;
-    base = (base*base) % modulus;
+    e = e >> 1;
+    b = (b*b) % m;
   }
 
   return result;
+
+  //return result.to_ap_uint();
 }
 
 
