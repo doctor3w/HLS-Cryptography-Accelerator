3c63f7251cd2d587d8e1845a9f1f8b72b409919e
Jacob Glueck <jng55@cornell.edu>
2018-11-28 10:20:36 -0500

    remove fixed-size vector

--- rsa/bignum.h
+++ rsa/bignum.h
@@ -5,42 +5,28 @@
 
 #include <stdint.h>
 #include "big_ap_int.h"
-#include "vector.h"
 #include "pragmas.h"
 
 template <int MAX_DIGITS, int BITS>
 class Bignum {
  public:
+  typedef ap_uint<MAX_DIGITS* BITS> BigAp;
+  typedef ap_uint<MAX_DIGITS* BITS> Internal;
   typedef ap_uint<BITS> Digit;
   typedef ap_uint<2*BITS> Wigit;
 
-  Bignum(ap_uint<MAX_DIGITS* BITS> value = 0) {
-  HLS_PRAGMA(inline);
-  INIT_LOOP:
-    for (int i = 0; i < MAX_DIGITS; i++) {
-      HLS_PRAGMA(unroll);
-      digits.push_back(value(BITS - 1 + i* BITS, i*BITS));
-    }
-    trim();
-  }
-
-  ap_uint<MAX_DIGITS * BITS> to_ap_uint() {
-  HLS_PRAGMA(inline);
-    ap_uint<MAX_DIGITS* BITS> result = 0;
-CONVERT_DIGITS: for (int i = 0; i < MAX_DIGITS; i++) {
-      HLS_PRAGMA(unroll);
-      if (i >= digits.size()) break;
-CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
-      HLS_PRAGMA(unroll);
-        result[i * BITS + b] = digits[i][b];
-      }
-    }
-    return result;
-  }
-
-  int operator[](int index) {
+  Bignum(BigAp value = 0) : digits(value) {
     HLS_PRAGMA(inline);
-    return digits[index / BITS][index % BITS];
+  }
+
+  BigAp to_ap_uint() {
+    HLS_PRAGMA(inline);
+    return digits;
+  }
+
+  int operator[](int index) const {
+    HLS_PRAGMA(inline);
+    return digits[index];
   }
 
   friend Bignum operator+(Bignum u, const Bignum& v) {
@@ -50,29 +36,7 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
   }
 
   Bignum& operator+=(const Bignum& rhs) {
-    const int n = rhs.digits.size();
-    if (digits.size() < n) {
-      digits.resize(n, 0);
-    }
-    int j = 0;
-    Wigit k = 0;
-    for (; j < MAX_DIGITS; ++j) {
-      HLS_PRAGMA(unroll);
-      if (j >= n) break;
-      k = k + digits[j] + rhs.digits[j];
-      digits[j] = static_cast<Digit>(k);
-      k >>= BITS;
-    }
-    for (; k != 0 && j < MAX_DIGITS; ++j) {
-      HLS_PRAGMA(unroll);
-      if (j >= digits.size()) break;
-      k += digits[j];
-      digits[j] = static_cast<Digit>(k);
-      k >>= BITS;
-    }
-    if (k != 0) {
-      digits.push_back(1);
-    }
+    digits += rhs.digits;
     return *this;
   }
 
@@ -83,31 +47,41 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
   }
 
   Bignum& operator-=(const Bignum& rhs) {
-    int j = 0;
-    Wigit k = 0;
-    for (; j < MAX_DIGITS; ++j) {
-      HLS_PRAGMA(unroll);
-      if (j >= rhs.digits.size()) break;
-      k = k + digits[j] - rhs.digits[j];
-      digits[j] = static_cast<Digit>(k);
-      k = ((k >> BITS) ? -1 : 0);
-    }
-    for (; k != 0 && j < MAX_DIGITS; ++j) {
-      HLS_PRAGMA(unroll);
-      if (j >= digits.size()) break;
-      k += digits[j];
-      digits[j] = static_cast<Digit>(k);
-      k = ((k >> BITS) ? -1 : 0);
-    }
-    trim();
+    digits -= rhs.digits;
     return *this;
   }
 
+  void set_block(int i, Digit v) {
+    HLS_PRAGMA(inline);
+    if (i < MAX_DIGITS) {
+      digits(i * BITS + BITS - 1, i* BITS) = v(BITS - 1, 0);
+    }
+  }
+  
+  Digit block(int i) const {
+    HLS_PRAGMA(inline);
+    if (i < MAX_DIGITS) {
+      return digits(i * BITS + BITS - 1, i* BITS);
+    } else {
+      return 0;
+    }
+  }
+
+  int size() const {
+    HLS_PRAGMA(inline);
+    for (int result = MAX_DIGITS - 1; result >= 0; result--) {
+      HLS_PRAGMA(unroll);
+      if (block(result) != 0) {
+        return result + 1;
+      }
+    }
+    return 1;
+  }
+
   friend Bignum operator*(const Bignum& u, const Bignum& v) {
-    const int m = u.digits.size();
-    const int n = v.digits.size();
+    const int m = u.size();
+    const int n = v.size();
     Bignum w;
-    w.digits.resize(m + n, 0);
   OUTER:
     for (int j = 0; j < MAX_DIGITS; ++j) {
       if (j >= n) break;
@@ -116,13 +90,12 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
       for (int i = 0; i < MAX_DIGITS; ++i) {
         HLS_PRAGMA(unroll);
         if (i >= m) break;
-        k += static_cast<Wigit>(u.digits[i]) * v.digits[j] + w.digits[i + j];
-        w.digits[i + j] = static_cast<Digit>(k);
+        k += static_cast<Wigit>(u.block(i)) * v.block(j) + w.block(i + j);
+        w.set_block(i + j, static_cast<Digit>(k));
         k >>= BITS;
       }
-      w.digits[j + m] = static_cast<Digit>(k);
+      w.set_block(j + m, static_cast<Digit>(k));
     }
-    w.trim();
     return w;
   }
 
@@ -162,15 +135,15 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
 
   void divide(Bignum v, Bignum& q, Bignum& r) const {
     r.digits = digits;
-    const int n = v.digits.size();
-    if (digits.size() < n) {
-      q.digits.assign(1, 0);
+    const int n = v.size();
+    if (size() < n) {
+      q.digits = 0;
       return;
     }
 
     // Normalize divisor (v[n-1] >= BASE/2).
     unsigned d = BITS;
-    Digit vn = v.digits.back();
+    Digit vn = v.block(n - 1);
   NORMALIZE:
     for (int magic = 0; magic < BITS; magic++) {
       HLS_PRAGMA(unroll);
@@ -180,14 +153,12 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
     }
     v <<= d;
     r <<= d;
-    vn = v.digits.back();
+    vn = v.block(n - 1);
 
     // Ensure first single-digit quotient (u[m-1] < v[n-1]).
-    r.digits.push_back(0);
-    const int m = r.digits.size();
-    q.digits.resize(m - n);
+    const int m = r.size() + 1;
+    q.digits = 0;
     Bignum w;
-    w.digits.resize(n + 1);
     const Wigit MAX_DIGIT = (static_cast<Wigit>(1) << BITS) - 1;
     int j = m - n;
   DIVIDE:
@@ -196,7 +167,7 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
       j--;
       // Estimate quotient digit.
       Wigit a =
-          (static_cast<Wigit>(r.digits[j + n]) << BITS | static_cast<Wigit>(r.digits[j + n - 1])) /
+          (static_cast<Wigit>(r.block(j + n)) << BITS | static_cast<Wigit>(r.block(j + n - 1))) /
           vn;
       Wigit qhat = a < MAX_DIGIT ? a : MAX_DIGIT;
 
@@ -206,11 +177,11 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
       for (int i = 0; i < MAX_DIGITS; ++i) {
         HLS_PRAGMA(unroll);
         if (i >= n) break;
-        k += qhat * v.digits[i];
-        w.digits[i] = static_cast<Digit>(k);
+        k += qhat * v.block(i);
+        w.set_block(i, static_cast<Digit>(k));
         k >>= BITS;
       }
-      w.digits[n] = static_cast<Digit>(k);
+      w.set_block(n, static_cast<Digit>(k));
 
       // Check if qhat is too large (u - w < 0).
       bool is_trial = true;
@@ -223,12 +194,12 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
       COMPARE:
         for (int y = 0; y < MAX_DIGITS; y++) {
           HLS_PRAGMA(unroll);
-          if (i == 0 || r.digits[j + i] != w.digits[i]) {
+          if (i == 0 || r.block(j + i) != w.block(i)) {
             break;
           }
           i--;
         }
-        if ((is_trial = (r.digits[j + i] < w.digits[i]))) {
+        if ((is_trial = (r.block(j + i) < w.block(i)))) {
           // Adjust partial product (w -= v).
           --qhat;
           k = 0;
@@ -236,14 +207,14 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
           for (int i = 0; i < MAX_DIGITS; ++i) {
             HLS_PRAGMA(unroll);
             if (i >= n) break;
-            k = k + w.digits[i] - v.digits[i];
-            w.digits[i] = static_cast<Digit>(k);
+            k = k + w.block(i) - v.block(i);
+            w.set_block(i, static_cast<Digit>(k));
             k = ((k >> BITS) ? -1 : 0);
           }
-          w.digits[n] = static_cast<Digit>(k + w.digits[n]);
+          w.set_block(n, static_cast<Digit>(k + w.block(n)));
         }
       }
-      q.digits[j] = static_cast<Digit>(qhat);
+      q.set_block(j, static_cast<Digit>(qhat));
 
       // Compute partial remainder (u -= w).
       k = 0;
@@ -251,15 +222,18 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
       for (int i = 0; i < MAX_DIGITS; ++i) {
         HLS_PRAGMA(unroll);
         if (i >= n) break;
-        k = k + r.digits[j + i] - w.digits[i];
-        r.digits[j + i] = static_cast<Digit>(k);
+        k = k + r.block(j + i) - w.block(i);
+        r.set_block(j + i, static_cast<Digit>(k));
         k = ((k >> BITS) ? -1 : 0);
       }
     }
 
+    for (int x = 0; x < MAX_DIGITS; x++) {
+      if (x >= n) {
+        r.set_block(x, 0);
+      }
+    }
     // Denormalize remainder.
-    q.trim();
-    r.digits.resize(n);
     r >>= d;
   }
 
@@ -270,24 +244,7 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
   }
 
   Bignum& operator<<=(int rhs) {
-    HLS_PRAGMA(function_instantiate variable=rhs);
-    if (digits.back() != 0 && rhs != 0) {
-      const int n = rhs / BITS;
-      digits.insert(0, n, 0);
-      rhs -= n * BITS;
-      Wigit k = 0;
-    SHIFT:
-      for (int j = 0; j < MAX_DIGITS; ++j) {
-        HLS_PRAGMA(unroll);
-        if (j + n >= digits.size()) break;
-        k |= static_cast<Wigit>(digits[j + n]) << rhs;
-        digits[j + n] = static_cast<Digit>(k);
-        k >>= BITS;
-      }
-      if (k != 0) {
-        digits.push_back(static_cast<Digit>(k));
-      }
-    }
+    digits <<= rhs;
     return *this;
   }
 
@@ -298,24 +255,7 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
   }
 
   Bignum& operator>>=(int rhs) {
-    const int n = rhs / BITS;
-    if (n >= digits.size()) {
-      digits.assign(1, 0);
-    } else {
-      digits.erase(0, n);
-      rhs -= n * BITS;
-      Wigit k = 0;
-      int j = digits.size();
-      for (int x = 0; x < MAX_DIGITS; x++) {
-        HLS_PRAGMA(unroll);
-        if (j == 0) break;
-        j--;
-        k = k << BITS | digits[j];
-        digits[j] = static_cast<Digit>(k >> rhs);
-        k = static_cast<Digit>(k);
-      }
-      trim();
-    }
+    digits >>= rhs;
     return *this;
   }
 
@@ -326,31 +266,12 @@ CONVERT_BITS: for (unsigned int b = 0; b < BITS; b++) {
   }
 
   Bignum& operator^=(const Bignum& rhs) {
-    const int n = rhs.digits.size();
-    if (digits.size() < n) {
-      digits.resize(n, 0);
-    }
-    for (int j = 0; j < MAX_DIGITS; ++j) {
-      HLS_PRAGMA(unroll);
-      if (j >= n) break;
-      digits[j] ^= rhs.digits[j];
-    }
-    trim();
+    digits ^= rhs.digits;
     return *this;
   }
 
   friend bool operator<(const Bignum& u, const Bignum& v) {
-    const int m = u.digits.size();
-    int n = v.digits.size();
-    if (m != n) {
-      return (m < n);
-    }
-COMPARE: for (int x = 0; x < MAX_DIGITS; x++) {
-      HLS_PRAGMA(unroll);
-      n--;
-      if (n == 0 || u.digits[n] != v.digits[n]) break;
-    }
-    return (u.digits[n] < v.digits[n]);
+    return u.digits < v.digits;
   }
 
   friend bool operator>(const Bignum& u, const Bignum& v) {
@@ -377,25 +298,14 @@ COMPARE: for (int x = 0; x < MAX_DIGITS; x++) {
 
   // Return 1 + floor(log2(u)), or 0 for u == 0.
   int bits() const {
-    int count = (digits.size() - 1) * BITS;
-    for (Digit u = digits.back(); u != 0; u >>= 1, ++count) {
+    int s = size();
+    int count = (s - 1) * BITS;
+    for (Digit u = block(s - 1); u != 0; u >>= 1, ++count) {
       HLS_PRAGMA(unroll);
-     }
-    return static_cast<int>(count);
+    }
+    return count;
   }
 
  private:
-  vector<Digit, 1 + MAX_DIGITS> digits;
-
-  void trim() {
-  TRIM:
-    for (int x = 0; x < MAX_DIGITS; x++) {
-      HLS_PRAGMA(unroll);
-      if (digits.back() == 0 && digits.size() > 1) {
-        digits.pop_back();
-      } else {
-        break;
-      }
-    }
-  }
+  Internal digits;
 };
--- rsa/fpga_rsa.cc
+++ rsa/fpga_rsa.cc
@@ -16,20 +16,36 @@ ap_uint<MAX_BIT_LEN> fpga_powm(ap_uint<MAX_BIT_LEN> base,
     return 0;
   }
 
+  ap_uint<MAX_BIT_LEN> other = 1;
   RsaBignum result(1);
   RsaBignum b(base);
   RsaBignum e(exponent);
   RsaBignum m(modulus);
   RsaBignum zero(0);
+  assert(b.to_ap_uint() == base);
+  assert(e.to_ap_uint() == exponent);
+  assert(m.to_ap_uint() == modulus);
+  assert(zero.to_ap_uint() == 0);
+  assert(result.to_ap_uint() == other);
 
   b = b % m;
+  base = base % modulus;
+  assert(b.to_ap_uint() == base);
   POWM_LOOP: for (int i = 0; i < MAX_BIT_LEN; i++) {
     if (e > zero) {
       if (e[0] == 1) {
         result = (result * b) % m;
+        other = (other * base) % modulus;
+        assert(result.to_ap_uint() == other);
       }
       e = e >> 1;
-      b = (b * b) % m;
+      exponent = exponent >> 1;
+      assert(e.to_ap_uint() == exponent);
+      b = (b * b);
+      assert(b.to_ap_uint() == base * base);
+      b = b % m;
+      base = (base * base) % modulus;
+      assert(b.to_ap_uint() == base);
     }
   }
   return result.to_ap_uint();
