b2422cd7dd52d0f0656b9cd5bfdaebe2aa4a0b8b
daw268 <daw268@cornell.edu>
2018-11-29 22:19:40 -0500

    Disable unrolling

--- sha/ecelinux/run.tcl
+++ sha/ecelinux/run.tcl
@@ -31,9 +31,9 @@ create_clock -period 9
 set_directive_array_partition SHA512Hasher::SHA512Hasher buf -type cyclic -factor 8
 
 #set_directive_unroll read64clear/LOOP
-set_directive_unroll -factor 8 memcpy_u8/LOOP
-set_directive_unroll -factor 8 memset_u8/LOOP
-set_directive_unroll SHA512Hasher::digest/LOOP_U64
+#set_directive_unroll -factor 8 memcpy_u8/LOOP
+#set_directive_unroll -factor 8 memset_u8/LOOP
+#set_directive_unroll SHA512Hasher::digest/LOOP_U64
 #set_directive_unroll SHA512Hasher::byte_digest/LOOP_DIGEST -factor 2
 
 set_directive_array_partition SHA512Hasher::hashBlock W -type complete
