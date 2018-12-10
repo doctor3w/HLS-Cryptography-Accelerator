25707cfbf6d1d406c967fa792ae4feef45179b70
aik49 <aik49@cornell.edu>
2018-12-09 19:06:04 -0500

    FPGA and Host now pack bytes into uint32s instead of sending 1B at at ime

--- sha/ecelinux/main.cpp
+++ sha/ecelinux/main.cpp
@@ -1,51 +1,50 @@
-//==========================================================================
-// bnn.cpp
-//==========================================================================
-// @brief: A convolution kernel for CNN on digit recognition
-
 #include "unix_cracker.h"
 #include "typedefs.h"
 
 #include <hls_stream.h>
 #include <fstream>
-#include <iostream>
+#include <cstdio>
 #include <iomanip>
 
 using namespace std;
-
-//----------------------------------------------------------
-// Top function
-//----------------------------------------------------------
 
 void dut(
     hls::stream<ap_uint<32> > &strm_in,
     hls::stream<ap_uint<32> > &strm_out
 )
 {
-  uint8_t passwd[MAX_PWD_LEN+1];
-  uint8_t salt[MAX_SALT_LEN+1];
-  int s,p;
+  uint8_t passwd[MAX_PWD_LEN+1] = {};
+  uint8_t hash[HASH_LEN];
+  int p, i;
+  const uint8_t salt[MAX_SALT_LEN+1] = "8n./Hzqd";
+  const int s = MAX_SALT_LEN;
+  uint32_t passwd_bytes = 0, hash_bytes = 0;
 
-  // First read in the salt
-  for (s=0; s < sizeof(salt); s++) {
-    salt[s] = strm_in.read();
-    if (!salt[s]) break;
-  }
-
-  // Read in pass
+  // Receive the password
   for (p=0; p < sizeof(passwd); p++) {
-    passwd[p] = strm_in.read();
+    if (p % 4 == 0) {
+      passwd_bytes = strm_in.read();
+    } else {
+      passwd_bytes >>= 8;
+    }
+    passwd[p] = passwd_bytes;
     if (!passwd[p]) break;
   }
-
-  char hash[HASH_LEN];
 
   // Compute the hash
 	calc(hash, passwd, p, salt, s);
 
-  // Write the result out
-	for (int i=0; i < HASH_LEN; i++) {
-		strm_out.write(hash[i]);
+  //Send back the hash
+  for (int i = 0; i < HASH_LEN; i++) {
+    if (i % 4 == 0 && i >= 4) {
+      strm_out.write(hash_bytes);
+      hash_bytes = 0;
+    } else if (i % 4 != 0) {
+      hash_bytes >>= 8;
+    }
+    hash_bytes |= (uint32_t)hash[i] << 24;
   }
-
+ 
+  hash_bytes >>= 16;
+  strm_out.write(hash_bytes);
 }
--- sha/ecelinux/unix_cracker_test.cpp
+++ sha/ecelinux/unix_cracker_test.cpp
@@ -1,35 +1,83 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <sys/time.h>
 #include <hls_stream.h>
-#include "typedefs.h"
-//#include <stdlib.h> for crypt()
+#include <ap_int.h>
+
 #include "unix_cracker.h"
-// Check with echo -n "hello world" | sha512sum -t
 
 extern void dut(hls::stream<ap_uint<32> >&, hls::stream<ap_uint<32> >&);
 
-int main() {
-  // Note: if using crypt, prepend with $6$
-  const char salt[MAX_SALT_LEN+1] = "8n./Hzqd";
-  const char pass[MAX_PWD_LEN+1] = "This is my password!";
-  hls::stream<ap_uint<32> > in;
-  hls::stream<ap_uint<32> > out;
-  // Write salt
-  for (int i=0; i < strlen(salt)+1; i++) {
-    in.write(salt[i]);
+int main(int argc, char** argv)
+{
+  hls::stream<ap_uint<32> > in("in");
+  hls::stream<ap_uint<32> > out("out");
+
+  // Open the passwords file for reading
+  FILE* fp;
+  size_t len = 0;
+  char* pass = NULL;
+  fp = fopen("/home/aik49/SuperAwesomeFastCryptoHLSAcceleratorThing/sha/ecelinux/passwords.txt", "r");
+  if (fp == NULL) {
+    printf("Error reading the passwords file!\n");
+    exit(-1);
   }
 
-  for (int i=0; i < strlen(pass)+1; i++) {
-    in.write(pass[i]);
+  char hash[86+1] = {};
+  uint32_t passwd_bytes, hash_bytes;
+  struct timespec t1, t2;
+  int num_passwords = 0, i;
+  float total_time = 0;
+  while ((getline(&pass, &len, fp) != -1) && num_passwords < 1) {
+    if (strlen(pass) > MAX_PWD_LEN) continue;
+ 
+    // Remove the newline at the end
+    pass[strlen(pass)-1] = 0;
+
+    printf("%s -> ", pass);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+ 
+    // Send all the password chars that can be sent in chunks of 4
+    passwd_bytes = 0;
+    for (i = 0; i < strlen(pass)+1; i++) {
+      if (i % 4 == 0 && i >= 4) {
+        in.write(passwd_bytes);
+        passwd_bytes = 0;
+      } else if (i % 4 != 0) {
+        passwd_bytes >>= 8;
+      }
+      passwd_bytes |= (uint32_t)pass[i] << 24;
+    }
+
+    // Send remaining bytes
+    passwd_bytes >>= 8 * (i % 4);
+    in.write(passwd_bytes);
+
+    dut(in, out);
+
+    // Read the hash from the fpga
+    for (i = 0; i < HASH_LEN; i++) {
+      if (i % 4 == 0) {
+        hash_bytes = out.read();
+      } else {
+        hash_bytes >>= 8;
+      }
+      hash[i] = hash_bytes;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+    total_time += float(t2.tv_sec - t1.tv_sec) + (float(t2.tv_nsec)*1e-9 - float(t1.tv_nsec)*1e-9);
+    printf("%s\n", hash);
+    num_passwords++;
   }
 
-  dut(in, out);
-  
-  char hash[HASH_LEN+1];
-  hash[HASH_LEN] = 0;
-  for (int i=0; i < HASH_LEN; i++) {
-    hash[i] = out.read();
-  }
-  printf("Hash: %s\n", hash);
+  printf("Average time %fs\n", total_time/num_passwords);
+  printf("Passwords/second %f\n", 1/(total_time/num_passwords));
+
   return 0;
 }
